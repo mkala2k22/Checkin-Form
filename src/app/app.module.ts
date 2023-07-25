30e96426c67e6e8992f5dca968604c56eac64559
mkala2k22 <109620709+mkala2k22@users.noreply.github.com>
2023-07-26 01:15:50 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CheckinComponent } from './checkin/checkin.component';
 import { ReactiveFormsModule} from '@angular/forms';
 
-
 import { HttpClientModule} from '@angular/common/http';
+import { CheckinformComponent } from './checkinform/checkinform.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
 import { LanguageSelectorComponent } from './language-selector/language-selector.component';
+import { ApolloModule } from 'apollo-angular';
+import { GraphQLModule } from './graphql.module';
+
+import { CheckinComponent } from './checkin/checkin.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CheckinComponent,
+    CheckinformComponent,
     LanguageSelectorComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-     ReactiveFormsModule,
-     BrowserAnimationsModule,
-     HttpClientModule,
-     TranslocoRootModule
-     
+    HttpClientModule,
+    ReactiveFormsModule,
+    TranslocoRootModule,
+    GraphQLModule,
+    ApolloModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
