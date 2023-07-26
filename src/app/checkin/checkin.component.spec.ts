cc5a856237c2f267c1897034b9afc26981c86232
mkala2k22 <109620709+mkala2k22@users.noreply.github.com>
2023-07-26 21:50:20 +0530

    Update checkin.component.spec.ts

--- src/app/checkin/checkin.component.spec.ts
+++ src/app/checkin/checkin.component.spec.ts
@@ -1,16 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CheckinComponent } from './checkin.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Apollo, ApolloModule } from 'apollo-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { CheckinformComponent } from './checkinform.component';
-
-describe('CheckinformComponent', () => {
-  let component: CheckinformComponent;
-  let fixture: ComponentFixture<CheckinformComponent>;
+describe('CheckinComponent', () => {
+  let component: CheckinComponent;
+  let fixture: ComponentFixture<CheckinComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CheckinformComponent]
+      imports:  [HttpClientTestingModule, ApolloModule, FormsModule, ReactiveFormsModule],
+      declarations: [CheckinComponent]
     });
-    fixture = TestBed.createComponent(CheckinformComponent);
+    fixture = TestBed.createComponent(CheckinComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
