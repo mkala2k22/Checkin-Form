460d8b240d42e2d7a4ad3659ceb2912d34cdf30b
mkala2k22 <109620709+mkala2k22@users.noreply.github.com>
2023-07-26 21:51:26 +0530

    Update checkin-auth.service.spec.ts

--- src/app/service/checkin-auth.service.spec.ts
+++ src/app/service/checkin-auth.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CheckinAuthService } from './checkin-auth.service';
 
 describe('CheckinAuthService', () => {
   let service: CheckinAuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports:[ HttpClientTestingModule],
+    });
     service = TestBed.inject(CheckinAuthService);
   });
-
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
