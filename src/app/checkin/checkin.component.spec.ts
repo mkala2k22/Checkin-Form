02502a12e86402821ccad041e2bef1e1b106ae55
mkala2k22 <109620709+mkala2k22@users.noreply.github.com>
2023-07-26 09:49:17 +0530

    Update checkin.component.spec.ts

--- src/app/checkin/checkin.component.spec.ts
+++ src/app/checkin/checkin.component.spec.ts
@@ -1,82 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CheckinComponent } from './checkin.component';
-import { HttpClient } from '@angular/common/http';
-import { CheckinAuthService } from '../service/checkin-auth.service';
-import { Observable, of } from 'rxjs';
 
+import { CheckinformComponent } from './checkinform.component';
 
-describe('CheckinComponent', () => {
-  let component: CheckinComponent;
-  let fixture: ComponentFixture<CheckinComponent>;
-  let checkInServiceStub : any;
+describe('CheckinformComponent', () => {
+  let component: CheckinformComponent;
+  let fixture: ComponentFixture<CheckinformComponent>;
 
   beforeEach(() => {
-    checkInServiceStub = {
-      authenticateCheckin(): Observable<any[]>{
-        return of([
-          {
-            "status": "Success",
-            "message": "Successful Checkin",
-            "data": {
-                "bookingData": [
-                    {
-                        "bookingCode": 123456,
-                        "lastName": "Kala"
-                    },
-                    {
-                        "bookingCode": 223456,
-                        "lastName": "Kabadwal"
-                    },
-                    {
-                        "bookingCode": 323456,
-                        "lastName": "Kalia"
-                    }
-                ]
-            }
-        }
-        ]);
-      }
-    }
     TestBed.configureTestingModule({
-      declarations: [CheckinComponent],
-      providers: [
-        {provide : CheckinAuthService,
-         useValue: checkInServiceStub
-        }
-      ]
+      declarations: [CheckinformComponent]
     });
-    fixture = TestBed.createComponent(CheckinComponent);
+    fixture = TestBed.createComponent(CheckinformComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should get data from service ', () => {
-    component.proceedCheckin();
-    expect(component.content).toEqual([
-      {
-        "status": "Success",
-        "message": "Successful Checkin",
-        "data": {
-            "bookingData": [
-                {
-                    "bookingCode": 123456,
-                    "lastName": "Kala"
-                },
-                {
-                    "bookingCode": 223456,
-                    "lastName": "Kabadwal"
-                },
-                {
-                    "bookingCode": 323456,
-                    "lastName": "Kalia"
-                }
-            ]
-        }
-    }
-    ]);
   });
 });
